215410e9f501391b149c01cf2e7088f9276f0432
kanishkbagzai <kanishkbagzai23@gmail.com>
2024-07-29 20:06:44 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,14 @@
+public class second {
+    public static void main(String[] args) {
+        int a=10;
+        short b=32000;
+        long c=99900000;
+        byte d=126;//range -128 to 127
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+    }
+}
