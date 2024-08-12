a1de166980e8cba05d3873d027c0005558757392
kanishkbagzai <kanishkbagzai23@gmail.com>
2024-08-12 12:51:54 +0530

    Add files via upload

---
+++ seventh.java
@@ -0,0 +1,10 @@
+public class seventh {
+    public static void main(String[] args) {
+        String s="Java";
+        System.out.println(s.length());
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.substring(0,2));
+        
+    }
+}
