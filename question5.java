560d55ad1e540a4d759160d97dc25d81986c33e9
kanishkbagzai <kanishkbagzai23@gmail.com>
2024-09-11 18:39:58 +0530

    Add files via upload

---
+++ question5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class question5 {
+    public static void main(String[] args) {
+        
+    
+    Scanner sc = new Scanner(System.in);
+    double miles=sc.nextDouble();
+    double kilometers=(miles*1.60934);
+    System.out.println(kilometers+"Km1");
+}
+}
