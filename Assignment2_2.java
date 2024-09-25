e3e070e6267e93fdcd449f05d554f2f5336329a7
kanishkbagzai <kanishkbagzai23@gmail.com>
2024-09-25 18:39:47 +0530

    Add files via upload

---
+++ Assignment2_2.java
@@ -0,0 +1,12 @@
+public class Assignment2_2 {
+    public
+    Assignment2_2(){
+        System.out.println("Constructor with 0 arguements called");
+    }
+    
+    public static void main(String[] args) {
+        //zero argument constructor
+        Assignment2_2 obj=new Assignment2_2();
+
+    }
+}
